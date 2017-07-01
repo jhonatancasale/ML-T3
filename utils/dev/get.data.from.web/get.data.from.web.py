56445fcaf943e55c5b38bb74a840c27e190cb1eb
jhonatancasale <jhonatan@jhonatancasale.com>
2017-07-01 13:52:23 -0300

    Include additional phases, second call and first enrollment

--- utils/dev/get.data.from.web/get.data.from.web.py
+++ utils/dev/get.data.from.web/get.data.from.web.py
@@ -20,30 +20,32 @@ def main():
     logging.debug('Trying to get data of the last %d years', range_years)
 
     for year in years:
-        url = 'http://www.fuvest.br/vest{0}/download/FUVEST_{0}_qase_inscr_car_fuvest_{0}.pdf'.format(year)
-        logging.debug('Request year: %d to server', year)
+        common_url = 'http://www.fuvest.br/vest{0}/download/FUVEST_{0}_qase_{1}_car_fuvest_{0}.pdf'
+        urls = [common_url.format(year, phase) for phase in ['inscr', 'conv', '1matr']]
+        for url in urls:
+            logging.debug('Request year: %d to server', year)
 
-        filename = url.split('/')[-1]
-        if os.path.isfile(filename):
-            logging.debug('Skiping this, file already exists')
-            continue
+            filename = url.split('/')[-1]
+            if os.path.isfile(filename):
+                logging.debug('Skiping this, file already exists')
+                continue
 
-        try:
-            data = requests.get(url, stream=True)
-        except FileNotFoundError as e:
-            logging.debug('Fails because of %s', e)
-        else:
-            logging.debug('Done!')
+            try:
+                data = requests.get(url, stream=True)
+            except FileNotFoundError as e:
+                logging.debug('Fails because of %s', e)
+            else:
+                logging.debug('Done!')
 
-        logging.debug('Saving data on file %s', filename)
-        try:
-            f = open(filename, 'wb')
-            f.write(data.content)
-            f.close()
-        except IOError as e:
-            logging.debug('Fails because of %s', e)
-        else:
-            logging.debug('Done!')
+            logging.debug('Saving data on file %s', filename)
+            try:
+                f = open(filename, 'wb')
+                f.write(data.content)
+                f.close()
+            except IOError as e:
+                logging.debug('Fails because of %s', e)
+            else:
+                logging.debug('Done!')
 
     logging.debug('Finished')
 
